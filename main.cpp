14806dfc7dafab61730bed690f1afe21ac2577da
Thinker222 <es0197@yahoo.com>
2017-11-08 23:16:37 -0500

    -main.cpp edited to detail in comments the process of the entire OS

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // Loader, MMU, Dispatchers, scheduler, ready_queue (prio queue), io_queue, and done_queue
+    // and page_fault_queue are declared and initialized.
+
+    // Loader calls init();
+
+    // Thread for page fault and io begins
+
+    // Dispatcher threads begin
+
+    // Scheduling process starts in main thread
+
+    // Loop begins for scheduler. Continues while there are still jobs to be done
+
+    // Calls lt_sched
+
+    // Calls st_sched
+
+
     return 0;
 }
