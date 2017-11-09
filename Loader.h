cd30b87c5127bc729b0aaa14181c3bee9e4f50c1
Thinker222 <es0197@yahoo.com>
2017-11-08 22:59:38 -0500

    -Completed Loader header; needs approval

--- Loader.h
+++ Loader.h
@@ -2,7 +2,29 @@
 // Created by Eshin Griffith on 11/3/17.
 //
 
+/* The Loader class is used only at the beginning of main. Its only purpose is to call init()
+ * init(): Takes an mmu and a list of pcbs both by reference, and reads from the ProgramFile to
+ * fill those pcbs with proper information.
+ * Loader(): Creates a loader class
+ * ~Loader(): Deletes a loader class
+ * PROGRAM_FILE: The string containing the relative path to the ProgramFile
+ */
+
 #ifndef PHASE_2_LOADER_H
 #define PHASE_2_LOADER_H
+
+#include "Disk.h"
+#include "PCB.h"
+#include "Log.h"
+#include "MMU.h"
+
+class Loader {
+public:
+    void init(MMU &mmu, std::list<PCB *> &pcbs);
+    Loader();
+    ~Loader();
+private:
+    const std::string PROGRAM_FILE;
+};
 
 #endif //PHASE_2_LOADER_H
