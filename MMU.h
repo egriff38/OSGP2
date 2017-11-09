2510e2d6cdcfa637b90791eed9110f351ed0d9b6
Thinker222 <es0197@yahoo.com>
2017-11-08 22:47:15 -0500

    -Header for MMU complete; needs approval -Header for Disk comlete; needs approval -Header for RAM complete; needs approval

--- MMU.h
+++ MMU.h
@@ -4,8 +4,19 @@
 #ifndef PHASE_2_MMU_H
 #define PHASE_2_MMU_H
 
-/*
- *
+/* The MMU manages both the RAM and the Disk. No one besides the MMU is allowed to alter or read
+ * from the RAM or Disk!
+ * disk_frame_map: A page table map for the disk.
+ * ram_frame_map: A page table map for the Ram
+ * disk: The disk that the MMU manages
+ * ram: The RAM that the MMU manages
+ * ram_mutex, disk_mutex: Protect the read/write for disk and RAM respectively.
+ * disk_memory: An address integer and a string s is passed in. If s is NULL, then a read is done,
+ * and the read at the proper address is returned. If s is not NULL, the function writes the
+ * string s to the specified location. This function will fail if the address is out of bounds.
+ * add_page_to_ram: Does the exact same task as disk except it works with the RAM; see above.
+ * get_page_from_ram: This function takes a frame_num as an integer and returns a pointer array containing
+ * the page at that frame_num in ram.
  */
 
 #include <iostream>
@@ -20,13 +31,16 @@ private:
     std::map disk_frame_map<int, bool>;
     std::map ram_frame_map<int, bool>;
     Disk *disk;
-    // Ram *ram;
+    Ram *ram;
+    std::mutex ram_mutex;
+    std::mutex disk_mutex;
 
 public:
-    std::string disk_memory(int address, std::string = NULL);
-    std::string ram_memory(int address, std::string = NULL);
+    std::string disk_memory(int address, std::string s = NULL);
+    std::string ram_memory(int address, std::string s = NULL);
     bool add_page_to_disk(std::string word[],int frame_num);
-    bool add_page_to_ram();
+    bool add_page_to_ram(std::string word[], int frame_num);
+    std::string* get_page_from_ram(int frame_num) const;
 
 };
 #endif //PHASE_2_MMU_H
--- Ram.h
+++ Ram.h
@@ -2,7 +2,36 @@
 // Created by conrad on 11/7/17.
 //
 
+/* This is the RAM class which is controlled by an MMU object. No one but the MMU can manage this.
+ * read: returns a string at a specific address.
+ * write: writes a data to a specific address.
+ * write: writes a vector of strings to a specific address
+ * SIZE: The total number of RAM locations
+ * Ram(): The basic constructor for RAM
+ * ~RAM(): Deletes the RAM object; no other cleanup required
+ */
+
 #ifndef PHASE_2_RAM_H
 #define PHASE_2_RAM_H
+#include <mutex>
+#include <vector>
+
+class Ram
+{
+    public:
+        std::string read(int address);
+        void write(int address, std::string data);
+        void write(int address, std::vector<std::string> s);
+        const static int SIZE = 1024;
+        Ram();
+        ~RAM();
+
+    private:
+        std::string ram_data[SIZE];
+        int ram_used;
+};
+
+
 
 #endif //PHASE_2_RAM_H
+
