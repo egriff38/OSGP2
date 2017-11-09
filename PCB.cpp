0e5972dc39813ebed6c0e322c32b55ec727b2fff
zauzenne <32309357+zauzenne@users.noreply.github.com>
2017-11-08 21:52:09 -0500

    PCB h and cpp and include Log.h

--- PCB.cpp
+++ PCB.cpp
@@ -2,3 +2,52 @@
 // Created by conrad on 11/7/17.
 //
 
+
+#include <iostream>
+#include "PCB.h"
+
+//Prints out the PCB information
+void print_pcbs(std::list<PCB *> p)
+{
+
+    for(PCB *pcb : p)
+    {
+        std::cout << "ID\t" << pcb->job_id << std::endl;
+        std::cout << "Job Size:\t" << pcb->job_size << std::endl;
+        std::cout << "JobPri:\t" << pcb->job_pri << std::endl;
+        std::cout << "InSize:\t" << pcb->in_buf_size << std::endl;
+        std::cout << "OutSize:\t" << pcb->out_buf_size << std::endl;
+        std::cout << "TempSize:\t" << pcb->temp_buf_size << std::endl;
+        std::cout << "Total Size:\t" << pcb->total_size << std::endl;
+        std::string s;
+        switch((int)pcb->state)
+        {
+            case 0: {
+                s = "RUNNING";
+                break;
+            }
+            case 1: {
+                s = "READY";
+                break;
+            }
+            case 2: {
+                s = "BLOCKED";
+                break;
+            }
+            case 3: {
+                s = "NEW";
+                break;
+            }
+            case 4: {
+                s = "COMPLETED";
+                break;
+            }
+        }
+        std::cout << "State:\t" << s << std::endl;
+        std::cout << std::endl;
+    }
+
+    if (p.empty())
+        std::cout << ("No PCBs to print.") << std::endl;
+
+}
--- PCB.h
+++ PCB.h
@@ -5,4 +5,35 @@
 #ifndef PHASE_2_PCB_H
 #define PHASE_2_PCB_H
 
+#include<list>
+#include "Log.h"
+#include <map>
+
+//Process Control Block - Contains job information
+struct PCB {
+    //Job
+    int job_id;
+    int job_size;
+    int job_pri;
+    int in_buf_size;
+    int out_buf_size;
+    int temp_buf_size;
+    int total_size;
+    enum PROCESS_STATUS {RUNNING, READY, BLOCKED, NEW, COMPLETED};
+    enum PROCESS_STATUS state;
+    std::map<int,int> pageTable;
+    int job_disk_address;
+    int job_ram_address;
+    int prgm_counter;
+    int registers[16];
+    Log *wait_time;
+    Log *comp_time;
+
+    //Data
+    int data_disk_address;
+
+};
+
+void print_pcbs(std::list<PCB *> p);
+
 #endif //PHASE_2_PCB_H
