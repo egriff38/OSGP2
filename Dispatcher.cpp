88405874602570007d96db6f32eef23f8bd905ca
Thinker222 <es0197@yahoo.com>
2017-11-10 19:43:35 -0500

    - Wrote Dispatcher.cpp; needs testing with mmu based cpu - changed cmakelist.txt to cpp 14 standard to support std::chrono_literals

--- Dispatcher.cpp
+++ Dispatcher.cpp
@@ -1,4 +1,32 @@
 //
 // Created by conrad on 11/7/17.
 //
+#include "iostream"
+#include "Dispatcher.h"
+#include "CPU.h"
+#include <thread>
+#include <chrono>
+static void Dispatcher::start(MMU *mmu, M_priority_queue<PCB *> *ready_queue, M_queue<PCB *> *io_queue,
+                       M_queue<PCB *> *pf_queue, M_queue<PCB *> *done_queue) {
+    using namespace std::chrono_literals;
+     CPU *cpu = new CPU(mmu, production);
+     PCB *current;
+    while(done_queue->getSize() != 30)
+     {
 
+          current = ready_queue->pop();
+         if(current != nullptr)
+         {
+             current->state = PCB::RUNNING;
+             cpu->load_pcb(current);
+             while(cpu->state.state == PCB::RUNNING)
+                 cpu->Operate();
+             cpu->store_pcb();
+             done_queue->push(current);
+         }
+
+         std::this_thread::sleep_for(2ns);
+     }
+
+
+}
