4f96ad07831930cfc882584957b1aac1b8eb8866
cmcdermitt <charliemc1996@gmail.com>
2017-11-15 20:56:28 -0500

    Updated M_priority_queue to sort correctly

--- Mutex_queues.cpp
+++ Mutex_queues.cpp
@@ -1,8 +1,4 @@
 //
-// Created by conrad on 11/9/17.
-//
-
-//
 // Created by Charlie McDermitt on 11/8/17.
 //
 
@@ -11,8 +7,10 @@
 
 #include <mutex>
 #include <queue>
+#include <list>
 #include <functional>
 #include <iostream>
+#include "PCB.h"
 
 template<class T>
 class M_queue {
@@ -64,14 +62,26 @@ private:
 template<class U>
 class M_priority_queue {
 public:
+    static bool comp_priority(PCB *p1, PCB *p2) {
+        return p1->job_pri > p2->job_pri;
+    }
+
+    static bool comp_sjf(PCB *p1, PCB *p2) {
+        return p1->job_size > p2->job_size;
+    }
+
     //  typename compare = std::less<U>;
     M_priority_queue<U>() {
-        data = std::priority_queue<U>();
+        data = std::list<U>();
     }
 
     void push(U input) {
         mutex.lock();
-        data.push(input);
+        data.push_back(input);
+        if (PCB::SORT == PCB::SORTING_METHOD::PRIO)
+            data.sort(comp_priority);
+        else if (PCB::SORT == PCB::SORTING_METHOD::SJF)
+            data.sort(comp_sjf);
         mutex.unlock();
     }
 
@@ -81,8 +91,8 @@ public:
             mutex.unlock();
             return nullptr;
         }
-        U u = data.top();
-        data.pop();
+        U u = data.front();
+        data.pop_front();
         mutex.unlock();
         return u;
     }
@@ -95,10 +105,12 @@ public:
     }
 
 private:
-    std::priority_queue<U> data;
+    std::list<U> data;
     std::mutex mutex;
-
 };
+
+
+
 
 #endif //PHASE_2_MUTEX_QUEUES_H
 
