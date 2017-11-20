510eaae781258eea290fbecfa525576a124d85be
cmcdermitt <charliemc1996@gmail.com>
2017-11-20 14:08:45 -0500

    Worked on blocking queue

--- main.cpp
+++ main.cpp
@@ -5,33 +5,32 @@
 #include "PCB.h"
 #include "MMU.h"
 #include "Scheduler.h"
-#include "Dispatcher.cpp"
+#include "Dispatcher.h"
 #include "Mutex_queues.cpp"
 #include "CPU.h"
 
 int main() {
-const int CPU_NUM = 4;
+    const int CPU_NUM = 4;
 
 // Loader, MMU, Dispatchers, scheduler, ready_queue (prio queue), io_queue, and done_queue
-// and page_fault_queue are declared and initialized.
-auto pcbs = new M_priority_queue<PCB*>();
-auto ready_queue = new M_priority_queue<PCB*>();
-auto readyish_queue = new M_queue<PCB*>;
-auto io_queue = new M_queue<PCB*>();
-auto pf_queue = new M_queue<PCB*>();
-auto done_queue = new M_queue<PCB*>();
-auto mmu = new MMU();
-auto still_has_work = new bool(true);
-auto loader = new Loader();
-auto scheduler = new Scheduler(*pcbs,*ready_queue,*done_queue, *mmu);
-auto threads = std::vector<std::thread>();
+// and are declared and initialized.
+    auto pcbs = new M_priority_queue<PCB *>();
+    auto ready_queue = new M_priority_queue<PCB *>();
+    auto readyish_queue = new M_queue<PCB *>;
+    auto blocked_queue = new M_queue<blocking_info *>;
+    auto done_queue = new M_queue<PCB *>();
+    auto mmu = new MMU();
+    auto still_has_work = new bool(true);
+    auto loader = new Loader();
+    auto scheduler = new Scheduler(*pcbs, *ready_queue, *done_queue, *mmu);
+    auto threads = std::vector<std::thread>();
     // Loader calls init();
-    loader->init(*mmu,*pcbs);
+    loader->init(*mmu, *pcbs);
 
     // Dispatcher threads begin
 //    for(int i = 0; i < CPU_NUM; i++) {
 //
-//        std::thread(Dispatcher::start,mmu,ready_queue,io_queue,pf_queue,done_queue,i).detach();
+//        std::thread(Dispatcher::start,mmu,ready_queue,blocked_queue,done_queue,i).detach();
 //    }
 
     //Blocking thread will start here
@@ -47,7 +46,8 @@ auto threads = std::vector<std::thread>();
 
 
     // Thread for page fault and io begins (later)
-    std::cout << scheduler->getDone()<<" Finish";
+    std::cout << scheduler->getDone() << " Finish";
 
     return 0;
 }
+
