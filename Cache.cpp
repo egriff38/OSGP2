fb67c200838d4055c608c3d7d9a74ff73167dfbc
cmcdermitt <charliemc1996@gmail.com>
2017-11-20 18:36:39 -0500

    Updated cache for paging - broke CPU

--- Cache.cpp
+++ Cache.cpp
@@ -5,26 +5,46 @@
 #include "Cache.h"
 #include <cstdlib>
 
-std::string Cache::read(int address) {
-    std::string rd = cache_data[address];
-    return rd;
+std::vector<std::string> Cache::read_instructions(int page_num) {
+    int cache_page_num = std::get<0>(instruction_words[page_num % INSTRUCTION_CACHE_SIZE]);
+    if (cache_page_num == page_num) {
+        std::vector<std::string> rd = std::get<1>(instruction_words[page_num % INSTRUCTION_CACHE_SIZE]);
+        return rd;
+    } else {
+        std::vector<std::string> rd = {"", "", "", ""}; //if the page isn't in the cache
+        return rd;
+    }
 }
 
-void Cache::write(int address, std::string data) {
-    if (address >= SIZE || address < 0)
-        exit(EXIT_FAILURE);
-    cache_data[address] = data;
-
+std::vector<std::string> Cache::read_data(int page_num) {
+    int cache_page_num = std::get<0>(data_words[page_num % DATA_CACHE_SIZE]);
+    if (cache_page_num == page_num) {
+        std::vector<std::string> rd = std::get<1>(data_words[page_num % DATA_CACHE_SIZE]);
+        return rd;
+    } else {
+        std::vector<std::string> rd = {"", "", "", ""}; //if the page isn't in the cache
+        return rd;
+    }
 }
 
-void Cache::write(int address, std::vector<std::string> data) {
-    for (int i = 0; i < data.size(); i++) {
-        this->cache_data[i + address] = data[i];
+void Cache::write_instructions(int page_num, std::vector<std::string> data) {
+    int cache_page_num = std::get<0>(instruction_words[page_num % INSTRUCTION_CACHE_SIZE]);
+    if (cache_page_num == page_num) { //if the page isn't already in the cache
+        std::get<1>(instruction_words[page_num % INSTRUCTION_CACHE_SIZE]) = data;
+    }
+}
+
+void Cache::write_data(int page_num, std::vector<std::string> data) {
+    int cache_page_num = std::get<0>(data_words[page_num % DATA_CACHE_SIZE]);
+    if (cache_page_num == page_num) { //if the page isn't already in the cache
+        std::get<1>(data_words[page_num % DATA_CACHE_SIZE]) = data;
     }
 }
 
 Cache::Cache() {
-    for (int i = 0; i < SIZE; i++)
-        cache_data[i] = "0";
-
+    std::vector<std::string> ev = {"", "", "", ""};
+    for (int i = 0; i < INSTRUCTION_CACHE_SIZE; i++)
+        instruction_words[i] = std::make_pair(-1, ev);
+    for (int i = 0; i < DATA_CACHE_SIZE; i++)
+        data_words[i] = std::make_pair(-1, ev);
 }
